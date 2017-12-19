ab84268bfc1fc9a0daa242d34450e2f6e2759db2
rafaelzabeu <rafaelzabeu@gmail.com>
2017-12-19 10:04:07 -0200

    Load screen?

---
+++ Assets/Scripts/LoadScreen/LoadScreenBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScreenBehaviour : MonoBehaviour {
+
+
+	
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
